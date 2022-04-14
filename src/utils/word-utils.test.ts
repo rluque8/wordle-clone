96ec47cc997372c6241bd8e851543752cbcb21d8
Rodrigo <rafarod8@gmail.com>
2022-04-14 18:14:05 +0200

    test: added more tests to algorithm matcher

--- src/utils/word-utils.test.ts
+++ src/utils/word-utils.test.ts
@@ -9,12 +9,42 @@ describe('Word-utils', () => {
 })
 
 describe('computeGuess', () => {
-  it('works with match and  present', () => {
+  it('works with match and present', () => {
     expect(computeGuess('boost','basic')).toEqual([
       LetterState.Match,
       LetterState.Miss,
       LetterState.Miss,
       LetterState.Present,
+      LetterState.Miss,
+    ]);
+  });
+  
+  it('works with full matches', () => {
+    expect(computeGuess('boost','boost')).toEqual([
+      LetterState.Match,
+      LetterState.Match,
+      LetterState.Match,
+      LetterState.Match,
+      LetterState.Match,
+    ]);
+  });
+  
+  it('works with full miss', () => {
+    expect(computeGuess('guard','boost')).toEqual([
+      LetterState.Miss,
+      LetterState.Miss,
+      LetterState.Miss,
+      LetterState.Miss,
+      LetterState.Miss,
+    ]);
+  });
+  
+  it('only does one match when two letters are present', () => {
+    expect(computeGuess('solid','boost')).toEqual([
+      LetterState.Present,
+      LetterState.Match,
+      LetterState.Miss,
+      LetterState.Miss,
       LetterState.Miss,
     ]);
   });
