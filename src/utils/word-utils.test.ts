4523e369534bff099c9d64fb1a1613eb0ab52218
Rodrigo <rafarod8@gmail.com>
2022-04-14 18:43:24 +0200

    feat: added full computeGuess algorithm with tests

--- src/utils/word-utils.test.ts
+++ src/utils/word-utils.test.ts
@@ -9,8 +9,12 @@ describe('Word-utils', () => {
 })
 
 describe('computeGuess', () => {
+  it('returns empty when length mismatch', () => {
+    expect(computeGuess('abc', 'basic')).toEqual([]);
+  });
+
   it('works with match and present', () => {
-    expect(computeGuess('boost','basic')).toEqual([
+    expect(computeGuess('boost', 'basic')).toEqual([
       LetterState.Match,
       LetterState.Miss,
       LetterState.Miss,
@@ -18,9 +22,9 @@ describe('computeGuess', () => {
       LetterState.Miss,
     ]);
   });
-  
+
   it('works with full matches', () => {
-    expect(computeGuess('boost','boost')).toEqual([
+    expect(computeGuess('boost', 'boost')).toEqual([
       LetterState.Match,
       LetterState.Match,
       LetterState.Match,
@@ -28,9 +32,9 @@ describe('computeGuess', () => {
       LetterState.Match,
     ]);
   });
-  
+
   it('works with full miss', () => {
-    expect(computeGuess('guard','boost')).toEqual([
+    expect(computeGuess('guard', 'boost')).toEqual([
       LetterState.Miss,
       LetterState.Miss,
       LetterState.Miss,
@@ -38,14 +42,54 @@ describe('computeGuess', () => {
       LetterState.Miss,
     ]);
   });
-  
+
   it('only does one match when two letters are present', () => {
-    expect(computeGuess('solid','boost')).toEqual([
+    expect(computeGuess('solid', 'boost')).toEqual([
       LetterState.Present,
       LetterState.Match,
       LetterState.Miss,
       LetterState.Miss,
       LetterState.Miss,
+    ]);
+  });
+
+  it('only does one match when 2 letters are present', () => {
+    expect(computeGuess('allol','smelt')).toEqual([
+      LetterState.Miss,
+      LetterState.Present,
+      LetterState.Miss,
+      LetterState.Miss,
+      LetterState.Miss,
+    ]);
+  });
+
+  it('does match when more than 2 letters are present', () => {
+    expect(computeGuess('aaaaa','abcde')).toEqual([
+      LetterState.Match,
+      LetterState.Miss,
+      LetterState.Miss,
+      LetterState.Miss,
+      LetterState.Miss,
+    ]);
+  });
+
+  it('does match when 2 letters are present in both words', () => {
+    expect(computeGuess('aaaaa','aacde')).toEqual([
+      LetterState.Match,
+      LetterState.Match,
+      LetterState.Miss,
+      LetterState.Miss,
+      LetterState.Miss,
+    ]);
+  });
+
+  it('does full match when same letter is present more than 2 times in both words', () => {
+    expect(computeGuess('ppppp','ppppp')).toEqual([
+      LetterState.Match,
+      LetterState.Match,
+      LetterState.Match,
+      LetterState.Match,
+      LetterState.Match,
     ]);
   });
 })
--- src/utils/word-utils.ts
+++ src/utils/word-utils.ts
@@ -14,18 +14,50 @@ export enum LetterState {
 export function computeGuess(guess: string, answer: string): LetterState[] {
   const result: LetterState[] = [];
 
+  if (answer.length !== guess.length) return result;
+
   const guessArray = guess.split("");
   const answerArray = answer.split("");
 
+  const letterCountStorage: Record<string, number> = letterCounter(answer);
+
   guessArray.forEach((letter: string, index: number) => {
+
     if (letter === answerArray[index]) {
-      result.push(LetterState.Match);
+
+      if (letterCountStorage[letter] > 0) {
+        result.push(LetterState.Match);
+      } else {
+        result.push(LetterState.Miss);
+      }
+      letterCountStorage[letter] -= 1;
+
     } else if (answerArray.includes(letter)) {
-      result.push(LetterState.Present);
+
+      if (letterCountStorage[letter] > 0) {
+        result.push(LetterState.Present);
+      } else {
+        result.push(LetterState.Miss);
+      }
+      letterCountStorage[letter] -= 1;
+
     } else {
       result.push(LetterState.Miss);
     }
   })
 
   return result;
+}
+
+function letterCounter(word: string) {
+  const letterCountStorage: Record<string, number> = {};
+  word.split("").map((letter: string) => {
+    if (letterCountStorage[letter]) {
+      letterCountStorage[letter] += 1;
+    } else {
+      letterCountStorage[letter] = 1;
+    }
+  });
+
+  return letterCountStorage;
 }
