43b6752a5eb68f6d3740862719624102fbe360eb
Rodrigo <rafarod8@gmail.com>
2022-04-14 18:08:43 +0200

    fix: fixed tests and started matchingLetter algorithm

--- src/utils/word-utils.test.ts
+++ src/utils/word-utils.test.ts
@@ -1,9 +1,21 @@
 import { describe, expect, it } from 'vitest'
-import { getTodaysWord } from './word-utils'
+import { computeGuess, getTodaysWord, LetterState } from './word-utils'
 
 describe('Word-utils', () => {
   it('get random word', () => {
     const wordSelected = getTodaysWord();
     expect(wordSelected.length).toEqual(5);
-  })
+  });
+})
+
+describe('computeGuess', () => {
+  it('works with match and  present', () => {
+    expect(computeGuess('boost','basic')).toEqual([
+      LetterState.Match,
+      LetterState.Miss,
+      LetterState.Miss,
+      LetterState.Present,
+      LetterState.Miss,
+    ]);
+  });
 })
--- src/utils/word-utils.ts
+++ src/utils/word-utils.ts
@@ -4,3 +4,28 @@ export const getTodaysWord = () => {
   const randomIndex = Math.floor(Math.random() * wordBank.valid.length);
   return wordBank.valid[randomIndex];
 }
+
+export enum LetterState {
+  Miss,
+  Present,
+  Match,
+}
+
+export function computeGuess(guess: string, answer: string): LetterState[] {
+  const result: LetterState[] = [];
+
+  const guessArray = guess.split("");
+  const answerArray = answer.split("");
+
+  guessArray.forEach((letter: string, index: number) => {
+    if (letter === answerArray[index]) {
+      result.push(LetterState.Match);
+    } else if (answerArray.includes(letter)) {
+      result.push(LetterState.Present);
+    } else {
+      result.push(LetterState.Miss);
+    }
+  })
+
+  return result;
+}
