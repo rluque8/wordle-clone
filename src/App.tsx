615e799b58cd0b6d477f5cf2f438b5c8bb8ddca4
Rodrigo <rafarod8@gmail.com>
2022-04-15 12:15:45 +0200

    feat: show answer on game  over

--- src/App.tsx
+++ src/App.tsx
@@ -63,6 +63,7 @@ export default function App() {
         <div role="modal" className='absolute bg-white left-0 right-0 top-1/4 p-6
           w-3/4 mx-auto rounded border border-gray-500  text-center'>
           Game Over!
+          <WordRow letters={state.answer}/>
 
           <button className="block border rounded border-green-500 bg-green-500 p-2 mt-4 mx-auto shadow"
             onClick={() => {
